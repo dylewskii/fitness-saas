4f4fe951b001b8c4723672811a99a1d691f790ec
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-08-06 14:30:26 +0300

    fix hero rounded look

--- src/components/cta-panel.tsx
+++ src/components/cta-panel.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function CTAPanel() {
   return (
     <section className="relative flex max-w-[1920px] flex-col items-center justify-center px-12 pb-6 sm:px-32">
-      <h2 className="text-3xl font-bold">Start Your Free Trial</h2>
-      <p className="pt-6 text-center text-2xl lg:w-[52rem]">
+      <h2 className="pb-6 text-3xl font-bold">Start Your Free Trial</h2>
+      <p className="text-center text-2xl lg:w-[52rem]">
         Fitnessio is the ultimate solution for coaches and club owners.
         Effortlessly track client subscriptions, payments, and packages, so you
         can focus on what matters most - helping your clients achieve their
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -79,7 +79,7 @@ export default function Hero() {
           />
         </div>
 
-        <div className="bg-background absolute -bottom-36 z-50 w-full rounded-t-3xl p-4 sm:hidden"></div>
+        <div className="bg-background absolute bottom-0 z-50 w-full rounded-t-3xl p-4 sm:hidden"></div>
       </div>
     </section>
   );
