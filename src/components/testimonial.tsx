2f31cde79bdcb2ce454a5bd8e0dd62b9002f05f1
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-08-07 18:20:47 +0300

    fix layout

--- src/components/testimonial.tsx
+++ src/components/testimonial.tsx
@@ -5,15 +5,15 @@ export default function Testimonial() {
       <div className="hsl(60,7%,94%)/10 absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
       <div className="mx-auto max-w-2xl lg:max-w-4xl">
         {/* LOGO HERE? */}
-        <figure className="mt-10">
+        <figure className="">
           <blockquote className="text-center text-2xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
             <p>
               &#34;No more spreadsheet nightmares! This app has made tracking
               payments and subscriptions a breeze.&#34;
             </p>
           </blockquote>
-          <figcaption className="mt-10">
-            <div className="mt-4 flex items-center justify-center space-x-3 text-base">
+          <figcaption className="pt-10">
+            <div className="mt-4 flex flex-col items-center justify-center space-x-3 text-base sm:flex-row">
               <div className="font-semibold text-gray-900">Denzel W.</div>
               <svg
                 width={3}
