e4ddbfae15f25e9397756e35823cf41d672fa98b
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-08-07 18:39:14 +0300

    add Features page

---
+++ src/app/features/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import FeatureOverview from "~/components/feature-overview";
+
+export default function FeaturesPage() {
+  return (
+    <div>
+      <FeatureOverview />
+    </div>
+  );
+}
