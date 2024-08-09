cfd33baf07024eca06103684779ff4329aa88790
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-08-09 21:36:58 +0300

    add padding Y

--- src/components/cta-panel.tsx
+++ src/components/cta-panel.tsx
@@ -3,7 +3,7 @@ import Button from "./ui/button";
 
 export default function CTAPanel() {
   return (
-    <section className="relative flex max-w-[1920px] flex-col items-center justify-center px-12 pb-6 sm:px-32">
+    <section className="relative flex max-w-[1920px] flex-col items-center justify-center px-12 pb-6 pt-24 sm:px-32">
       <h2 className="section-title pb-6">Start Your Free Trial</h2>
       <p className="py-6 text-center text-2xl lg:w-[52rem]">
         Fitnessio is the ultimate solution for coaches and club owners.
