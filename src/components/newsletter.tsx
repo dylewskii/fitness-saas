29a790c698a9b23e6690881f50bf8bb66b5ad043
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-08-12 23:43:33 +0300

    update buttong

--- src/components/newsletter.tsx
+++ src/components/newsletter.tsx
@@ -36,7 +36,7 @@ export default function Newsletter() {
         </p>
         <EmailInput />
 
-        <button className="bg-secondary rounded-3xl border-2 border-black px-8 py-3 text-lg transition hover:bg-[hsl(46,48%,45%)]/80">
+        <button className="bg-secondary rounded-3xl border-2 border-black px-8 py-3 text-lg font-bold transition hover:bg-[hsl(46,48%,45%)]/80">
           Subscribe
         </button>
       </div>
