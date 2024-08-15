8c0dd45fabb8543fc0bf5c57c6fad55d7e80642d
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-08-15 16:02:06 +0300

    feat: add contact us page

---
+++ src/app/help/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HelpPage() {
+  return (
+    <div className="flex items-center bg-white p-24">
+      <h1 className="section-title">Need Help?</h1>
+    </div>
+  );
+}
