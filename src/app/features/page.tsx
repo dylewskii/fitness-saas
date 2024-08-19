5f0ccbf4dcac6cfeb35f668a7c1125d2411e9c0e
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-08-19 19:35:20 +0300

    chore: remove className prop to fix build

--- src/app/features/page.tsx
+++ src/app/features/page.tsx
@@ -1,8 +1,8 @@
 import FeatureOverview from "~/components/features/feature-overview";
 
-export default function FeaturesPage({ className }: { className: string }) {
+export default function FeaturesPage() {
   return (
-    <div className={`rounded-b-3xl rounded-t-3xl bg-white py-6 ${className}`}>
+    <div className={`rounded-b-3xl rounded-t-3xl bg-white py-6`}>
       <FeatureOverview bgColor="white" />
     </div>
   );
