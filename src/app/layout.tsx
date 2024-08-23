a8a93c4fce8ffd7668c0fe63b46afdc0dccb6f77
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-08-23 23:40:40 +0300

    chore: wrap children w/ tanstack provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { type Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import Footer from "~/components/footer/footer";
 import TopNavWrapper from "~/components/topnav/top-nav-wrapper";
+import { TanstackProvider } from "~/providers/tanstack-provider";
 
 export const metadata: Metadata = {
   title: "Fitnessio",
@@ -22,9 +23,11 @@ export default function RootLayout({
         className={`${GeistSans.variable} max-w-[100%] overflow-x-hidden bg-black`}
       >
         <body>
-          <TopNavWrapper />
-          {children}
-          <Footer />
+          <TanstackProvider>
+            <TopNavWrapper />
+            {children}
+            <Footer />
+          </TanstackProvider>
         </body>
       </html>
     </ClerkProvider>
