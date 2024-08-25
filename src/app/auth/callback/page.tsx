044610e56896b78c716214920ea9e1bb85374ee3
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-08-25 13:14:59 +0300

    feat: add stripe payment redirect

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,21 +1,37 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
 import { checkAuthStatus } from "./actions";
+import { useUser } from "@clerk/nextjs";
 
 export default function CallbackPage() {
   const router = useRouter();
-  const { data, isLoading } = useQuery({
+  const { user, isLoaded: isClerkLoaded, isSignedIn } = useUser();
+  const { data, isLoading: isAuthStatusLoading } = useQuery({
     queryKey: ["auth-status"],
     queryFn: async () => await checkAuthStatus(),
   });
 
-  if (data?.success) {
-    router.push("/");
-  }
+  // redirects to stripe payment page if link available & auth status is loaded
+  useEffect(() => {
+    if (!isClerkLoaded || !isSignedIn || isAuthStatusLoading) {
+      return;
+    }
+
+    const userEmail = user?.emailAddresses?.[0]?.emailAddress;
+    const stripePaymentLink = localStorage.getItem("stripePaymentLink");
+
+    if (data?.success && stripePaymentLink && userEmail) {
+      const uriEncodedEmail = encodeURIComponent(userEmail);
+      localStorage.removeItem("stripePaymentLink");
+      router.push(`${stripePaymentLink}?prefilled_email=${uriEncodedEmail}`);
+    } else {
+      router.push("/");
+    }
+  }, [data, user, router, isClerkLoaded, isSignedIn, isAuthStatusLoading]);
 
   return (
     <div className="flex h-screen flex-col items-center justify-center text-white">
