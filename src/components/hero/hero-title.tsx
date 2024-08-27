e84c09ea9b7d9cc82162728020d8396d82d17c48
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-08-27 23:21:56 +0300

    style: fix title size & position on < sm devices

--- src/components/hero/hero-title.tsx
+++ src/components/hero/hero-title.tsx
@@ -6,7 +6,7 @@ export const HeroTitle = () => {
       per="word"
       as="h1"
       preset="blur"
-      className="bg-background block text-[2.5rem] font-bold tracking-tighter sm:my-auto sm:text-[3rem] md:text-[4rem] md:leading-[3.75rem] lg:text-[4.5rem]"
+      className="block bg-background text-center text-[2.5rem] font-bold tracking-tighter sm:my-auto sm:text-[3.5rem] md:text-start md:text-[4rem] md:leading-[3.75rem] lg:text-[4.5rem]"
     >
       Focus on your club. We&apos;ll take care of the rest.
     </TextEffect>
